6a2a78763bec3abe8d8c1a31c2b319e52da5cb73
shanakaperera <shanakaperera08@gmail.com>
2017-07-09 22:35:17 +0530

    mb string updated

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit34df6f002b8b483e742be0712b25a528
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit34df6f002b8b483e742be0712b25a528::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire34df6f002b8b483e742be0712b25a528($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire34df6f002b8b483e742be0712b25a528($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
