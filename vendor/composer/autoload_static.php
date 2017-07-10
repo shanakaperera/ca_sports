badb2c04e150ad0b5a10a7a86e9b74a3ee5ae337
shanakaperera <shanakaperera08@gmail.com>
2017-07-10 07:23:52 +0530

    Composer update 22

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,29 +6,9 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit34df6f002b8b483e742be0712b25a528
 {
-    public static $files = array (
-        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
-    );
-
-    public static $prefixLengthsPsr4 = array (
-        'S' => 
-        array (
-            'Symfony\\Polyfill\\Mbstring\\' => 26,
-        ),
-    );
-
-    public static $prefixDirsPsr4 = array (
-        'Symfony\\Polyfill\\Mbstring\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
-        ),
-    );
-
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit34df6f002b8b483e742be0712b25a528::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit34df6f002b8b483e742be0712b25a528::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
